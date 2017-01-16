2db212463a6d465137bc363086505bbcb43de39d
Parra_ <ricardoaimalmeida@gmail.com>
2017-01-16 02:39:48 +0000

    updated code with npcs lib

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"cloudnativego/section3/npcs"
+
+	"github.com/pardejini/npcs"
 )
 
 func main() {
-	mob := npcs.NonPlayerCharacter{ Name: "Alfred"}
+	mob := npcs.NonPlayerCharacter{Name: "Alfred"}
 	fmt.Println(mob)
 
 	hortense := npcs.NonPlayerCharacter{Name: "Hortense",
-		Loc: npcs.Location{X: 10.0, Y:10.0, Z:10.0}}
-	
+		Loc: npcs.Location{X: 10.0, Y: 10.0, Z: 10.0}}
+
 	fmt.Println(hortense)
-	fmt.Printf("Alfred is %f units from Hortense. \n", mob.DistanceTo(hortense) )
+	fmt.Printf("Alfred is %f units from Hortense. \n", mob.DistanceTo(hortense))
 }
